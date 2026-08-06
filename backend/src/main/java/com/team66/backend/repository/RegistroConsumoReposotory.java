cfc37d745cef88bc3316942d74cf4c109f0b00b2
Alon01 <13868197+Alon01@users.noreply.github.com>
2026-08-06 13:24:51 -0600

    feat: agregando RegistroConsumoRepository

---
+++ backend/src/main/java/com/team66/backend/repository/RegistroConsumoReposotory.java
@@ -0,0 +1,8 @@
+package com.team66.backend.repository;
+
+
+import com.team66.backend.model.RegistroConsumo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistroConsumoReposotory extends JpaRepository<RegistroConsumo, Long> {
+}
