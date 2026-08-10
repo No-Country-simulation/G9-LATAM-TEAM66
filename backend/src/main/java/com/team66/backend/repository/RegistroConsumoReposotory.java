8a615e4c9d5da9310702d45fe26e3ccff34a29f5
Alon01 <13868197+Alon01@users.noreply.github.com>
2026-08-10 13:39:48 -0600

    feat: agregando anotacion Repository.

--- backend/src/main/java/com/team66/backend/repository/RegistroConsumoReposotory.java
+++ backend/src/main/java/com/team66/backend/repository/RegistroConsumoReposotory.java
@@ -3,6 +3,8 @@ package com.team66.backend.repository;
 
 import com.team66.backend.model.RegistroConsumo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RegistroConsumoReposotory extends JpaRepository<RegistroConsumo, Long> {
 }
